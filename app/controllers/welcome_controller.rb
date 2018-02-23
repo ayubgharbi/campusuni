36e2f0b0707db1de32e391993b0cea7b0ccd50b7
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-02-23 22:28:18 +0000

    504

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,5 @@ class WelcomeController < ApplicationController
 skip_before_action :authorize
 
   def index
-  	@gegm = Deust.first
-  	@semesters = Semester.all.order("created_at ASC")
-  	@matieres = Matiere.all
   end
 end
