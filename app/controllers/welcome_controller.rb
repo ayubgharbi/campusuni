84d1120c17f0bc1f1bc4a212a334e2f4fa7cd3a4
ayoub gharbi <ayoub97ma@gmail.com>
2018-11-04 14:43:40 +0100

    desc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 skip_before_action :authorize
   def index
   	@posts = Post.last(4)
-  	@deusts = Deust.all
+  	@deusts = Deust.all.order("created_at DESC")
   end
 
 end
